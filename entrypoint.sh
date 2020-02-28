8c673003d9864160dd1b477a18873b80c88aaf0a
Le Anh Duc <ducla@lifull-tech.vn>
2020-02-28 14:47:07 +0700

    change exit status

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,17 +9,15 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
     --report-checkstyle=/tmp/phpcs_result_checkstyle.xml \
     -p -s --colors -d memory_limit=1G --ignore=vendor --extensions=php \
     --standard=./phpcs.xml \
-    --runtime-set ignore_errors_on_exit 1 \
-    --runtime-set ignore_warnings_on_exit 1 \
     ${INPUT_PHPCS_ARGS:-\.}
 
-EXIT_CODE=$?
+EXIT_CODE1=$?
 
 cat /tmp/phpcs_result_checkstyle.xml | reviewdog -f=checkstyle -name="phpcs" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
 
 /usr/local/bin/phpmd.phar ${INPUT_PHPMD_ARGS:-\.} xml ./phpmd.xml -dmemory_limit=-1 > /tmp/phpmd-report.xml
 
-EXIT_CODE=$?
+EXIT_CODE2=$?
 
 cat /tmp/phpmd-report.xml | reviewdog -f=checkstyle -name="phpmd" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
 
@@ -29,8 +27,8 @@ cat /tmp/phpmd-report.xml | reviewdog -f=checkstyle -name="phpmd" -reporter="${I
     ${INPUT_PHPSTAN_ARGS:-\.} \
     > /tmp/phpstan-report.xml
 
-EXIT_CODE=$?
+EXIT_CODE3=$?
 
 cat /tmp/phpstan-report.xml | reviewdog -f=checkstyle -name="phpstan" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
 
-exit $EXIT_CODE
+! (( $EXIT_CODE1 || $EXIT_CODE2 || $EXIT_CODE3 ))
